f87709ec0079ad1fb1c0a3c65dd1c9f17a350128
Kaywuyep <wuyepkate@gmail.com>
2023-08-12 12:44:42 -0700

    sample

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #define MAX_COMMAND_LENGTH 1024
+
+/*ssize_t getline(char **restrict lineptr, size_t *restrict n,
+		FILE *restrict stream);prototype from man 3 getline*/
+char *strtok(char *str, const char *delim);/*prototype from man strtok*/
+
+void execute_command(const char *command);
+
+#endif
--- simple_shell.c
+++ simple_shell.c
@@ -12,39 +12,3 @@ void execute_command(const char *command)
 		printf("Error executing command: %s\n", command);
 	}
 }
-/**
- * main - entry point
- *
- * Return: 0 sucess
- */
-int main(void)
-{
-	char command[MAX_COMMAND_LENGTH];
-	size_t command_len;
-
-	while (1)
-	{
-		/*Display the prompt, to request a command*/
-		printf("simple_shell:) ");
-		fflush(stdout);
-
-		/*Read the user input*/
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
-			/*Handle end of file (Ctrl+D)*/
-			break;
-		}
-
-		/*Remove the newline character*/
-		command_len = strlen(command);
-		if (command_len > 0 && command[command_len - 1] == '\n')
-		{
-			command[command_len - 1] = '\0';
-		}
-
-		/*Execute the command*/
-		execute_command(command);
-	}
-
-	return (0);
-}
